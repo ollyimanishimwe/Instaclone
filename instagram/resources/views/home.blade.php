a5d62ef2bafd84463f879883ec0fc6a14783e80e
Olly <imanishimweolyy@gmail.com>
2020-06-24 22:20:32 +0200

    the fronted pre designed

---
+++ instagram/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="row col-3 p-5">
+            <img src="svg/olly.jpg" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Olly Imanshimwe</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>44</strong> posts</div>
+                <div class="pr-5"><strong>100M</strong> followers</div>
+                <div class="pr-5"><strong>1</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
