468c17e17eb9e4751db712038a95aa312f4c1072
Olly <imanishimweolyy@gmail.com>
2020-06-24 23:41:06 +0200

    The Auth modified

--- instagram/resources/views/home.blade.php
+++ instagram/resources/views/home.blade.php
@@ -13,6 +13,20 @@
                 <div class="pr-5"><strong>100M</strong> followers</div>
                 <div class="pr-5"><strong>1</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">OllyImanishimwe</div>
+            <div>&Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas numquam omnis rerum quod eaque debitis totam ea rem deserunt. Atque assumenda a aperiam iste, error impedit soluta ratione molestias ipsam?</div>
+            <div><a href="#">www.imanishimwe.com/</a></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <img src="svg/olly.jpg" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="svg/olly.jpg" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="svg/olly.jpg" alt="" class="w-100">
         </div>
     </div>
 </div>
