ec84bced3c777c3b2794bd553d65433eca202fa0
Olly <imanishimweolyy@gmail.com>
2020-06-28 16:53:26 +0200

    the setting of new image path

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,13 @@ class Profile extends Model
 {
     protected $guarded = [];
     
+    public function profileImage(){
+
+        $imagePath = ($this->image) ?  $this->image : 'profile/o1HlE2HsphMJXeVNFtRNkotIs6GY23sBbtVbyhrR.jpeg';
+        
+        return '/storage/' . $imagePath; 
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
